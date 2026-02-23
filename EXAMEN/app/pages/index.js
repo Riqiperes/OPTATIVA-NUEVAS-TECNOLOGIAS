91d05be25fa68646cde7e7abaa0881aa1ef66441
Riqiperes <riqiperes14@gmail.com>
2026-02-23 11:22:53 -0600

    fix pipeline2

--- EXAMEN/app/pages/index.js
+++ EXAMEN/app/pages/index.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>¡Hola Mundo desde Next.js en Fargate!</h1>
-      <p>Este es el examen de primer parcial de Nuevas Tecnologías.</p>
+      <p>Este es el examen de primer parcial de Nuevas Tecnologías!.</p>
     </div>
   )
 }
